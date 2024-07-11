c93fc84f78ce89a2319cef8446297940b7b30598
JulianSaliqunaj <juliansaliqunaj8@gmail.com>
2024-07-11 11:51:05 +0200

    Add files via upload

---
+++ index2.py
@@ -0,0 +1,8 @@
+while True:
+    name = input("write username:")
+    if name != "juliansaliqunaj":
+                continue
+    password = input ("write the password:")
+    if password == "jcoders":
+            break
+    print("name and password are correct")
